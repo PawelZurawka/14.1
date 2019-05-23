a9fab59573ce64cb869fd837a9b3f1e623a3222d
Paweł Żurawka <pzurawka@gmail.com>
2019-05-23 18:01:09 +0200

    First task complete

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 14.1
+https://pawelzurawka.github.io/14.1/https://pawelzurawka.github.io/14.1/
---
+++ js/script.js
@@ -0,0 +1,27 @@
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  hash: true,
+  pageDots: false,
+});
+
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity( '.main-carousel', {
+  // options
+});
+
+//FUNCTION "BACK TO FIRST SLIDE"
+var firstSlide = document.querySelector('.restartButton');
+firstSlide.addEventListener('click', function () {
+  flkty.select(0);
+});
+
+//PROGRESS BAR
+var progressBar = document.querySelector('.progress-bar');
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
