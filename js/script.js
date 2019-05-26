610a7b36156a8161c77dd835fe8ed75ddbfddd82
Paweł Żurawka <pzurawka@gmail.com>
2019-05-26 19:17:31 +0200

    Add map and loop for markers

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+'use strict';
 //MUSTACHE
 (function() {
 var slides = document.getElementById('template-carousel-slides').innerHTML;
@@ -40,3 +41,29 @@ flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
+
+//GOOGLE MAPS
+(function(){ 
+	
+	// Definujemy funkcję initMap w zakresie globalnym (czyli jako właściwość obiektu window).
+  window.initMap = function() {
+		
+		// Zapisujemy w zmiennej obiekt zawierający współrzędne geograficzne.
+		//var uluru = {lat: 35.370566, lng: 24.477572};
+		
+		// W zmiennej map zapisujemy nową instancję obiektu Map. 
+		var map = new google.maps.Map(document.getElementById('map'), {
+			// Podajemy opcje mapy, np. zoom i punkt wycentrowania mapy.
+			center: mySlides[0].coords,
+			zoom: 4
+		});
+
+		for(var i = 0; i < mySlides.length; i++) {
+			var slide = mySlides[i];
+			var marker = new google.maps.Marker ({
+			position: slide.coords,
+			map: map
+			});
+		}
+	};	 
+})();  
