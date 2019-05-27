ee283eb9d3d2c562d4cc5f348fcff68c1a3bfb49
Paweł Żurawka <pzurawka@gmail.com>
2019-05-27 15:53:58 +0200

    Task done

--- js/script.js
+++ js/script.js
@@ -35,7 +35,40 @@ flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
+//INIT MAP
+(function(){ 
+  window.initMap = function() {
+		var map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 4,
+			center: mySlides[0].coords
+		});
 
+		//ADD MARKERS TO MAP
+    var markers = [];
+    for ( var i = 0; i < mySlides.length; i++ ) {
+      markers.push(new google.maps.Marker({
+        position: mySlides[i].coords,
+        map: map,
+        marker: i
+      }))
+      markers[i].addListener("click", function() {
+        flkty.select(this.marker);
+      });
+
+      //CHANGE SLIDE = CHANGE MARKER
+      flkty.on("change", function(index) {
+        map.panTo(mySlides[index].coords);
+        map.setZoom(8);
+      });
+    }
+  };
+
+
+
+
+
+
+/*
 //GOOGLE MAPS
 (function(){ 
 	
@@ -48,16 +81,25 @@ flkty.on( 'scroll', function( progress ) {
 		// W zmiennej map zapisujemy nową instancję obiektu Map. 
 		var map = new google.maps.Map(document.getElementById('map'), {
 			// Podajemy opcje mapy, np. zoom i punkt wycentrowania mapy.
-			center: mySlides[0].coords,
-			zoom: 4
+			zoom: 4,
+			center: mySlides[0].coords
 		});
-
+		//ADD MARKERS TO MAP
+		var markers = [];
 		for(var i = 0; i < mySlides.length; i++) {
-			var slide = mySlides[i];
-			var marker = new google.maps.Marker ({
-			position: slide.coords,
-			map: map
+			markers.push = (new google.maps.Marker ({
+			position: mySlides[i].coords,
+			map: map,
+			id: i
+			}));
+			markers[i].addListener('click', function() {
+				flkty.select(this.id);
+			});
+			// Change map position after slide change
+      flkty.on("change", function(index) {
+        map.panTo(slideData[index].coords);
+        map.setZoom(12);
 			});
 		}
-	};	 
+	};	*/ 
 })();  
